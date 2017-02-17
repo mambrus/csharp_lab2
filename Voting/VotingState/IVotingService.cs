517cc9303f962ccc67561eb6984ddfd4791b705b
Michael Ambrus <michael.ambrus@medius.se>
2017-02-17 15:50:39 +0100

    Completed ahead of course schedule: Part #17 & #14
    Implementation (copy & paste) ahead of course:
    
    VotingState::IVotingService
    VotingState::VoteData
    
    This permits compilation without errors as #10 depends on #17, which in
    turn depends on #14.

---
+++ Voting/VotingState/IVotingService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VotingState
+{
+    // Voting Service Operations interface.
+    // For lab purposes, the operations are included 
+    // in the service class. They should be separated
+    // out to allow for unit testing.
+    public interface IVotingService
+    {
+        // Gets the list of VotingData structures.
+        Task<IReadOnlyList<VotingData>> GetVotingDataAsync(string id, CancellationToken token);
+
+        // Updates the count on a vote or adds a new vote if it doesn't already exist.
+        Task AddVoteAsync(string key, int count, string id, CancellationToken token);
+
+        /// <summary>
+        /// Tracks the number of requests to the service across all controller instances.
+        /// </summary>
+        long RequestCount { get; set; }
+    }
+}
---
+++ Voting/VotingState/VoteData.cs
@@ -0,0 +1,48 @@
+﻿using Bond;
+using System;
+
+namespace VotingState
+{
+    // Defined as a Bond schema to assist with data versioning and serialization.
+    // Using a structure and read only properties to make this an immutable entity.
+    // This helps to ensure Reliable Collections are used properly.
+    // See https://docs.microsoft.com/en-us/azure/service-fabric/service-fabric-      
+    // work-with-reliable-collections
+    [Bond.Schema]
+    public struct VotingData
+    {
+        // Each field is attributed with an id that is unique. The field definition is never changed.
+        // This is the name of the vote, which will also be the key of the reliable dictionary.
+        [Bond.Id(0)]
+        public string Name { get; private set; }
+
+        // This is the current number of votes for this entity.
+        [Bond.Id(10)]
+        public int Count { get; private set; }
+
+        // This is the maximum number of votes for this entity.
+        [Bond.Id(20)]
+        public int MaxCount { get; private set; }
+
+        // DateTimeOffset is not a supported data type by Bond
+        // This is the date and time of the last vote.
+        [Bond.Id(30), Bond.Type(typeof(long))]
+        public DateTimeOffset LastVote { get; private set; }
+
+        // VotingData constructor.
+        public VotingData(string name, int count, int maxCount, DateTimeOffset date)
+        {
+            this.Name = name;
+            this.Count = count;
+            this.MaxCount = maxCount;
+            this.LastVote = date;
+        }
+
+        // Updates the count of a VotingData structure returning a new instance.
+        public VotingData UpdateWith(int count)
+        {
+            int newMax = Math.Max(this.MaxCount, count);
+            return new VotingData(this.Name, count, newMax, DateTimeOffset.Now);
+        }
+    }
+}
