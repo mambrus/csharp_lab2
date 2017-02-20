16ebefd11e36998866441c950e0e91e751444a7c
Michael Ambrus <michael.ambrus@medius.se>
2017-02-20 15:26:39 +0100

    Part #16 - VotingState::BondTypeAliasConverter added
    Static class added for date conversation back/forth into a integer
    representation to extend the abilities of the Bond class.

---
+++ Voting/VotingState/BondTypeAliasConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace VotingState
+{
+    /// <summary>
+    /// This is needed to support DateTimeOffset use in Bond schemas.
+    /// It converts the DateTimeOffset type to a long value and 
+    /// long values into DateTimeOffsets. See
+    /// https://microsoft.github.io/bond/manual/bond_cs.html#converter
+    /// </summary>
+    public static class BondTypeAliasConverter
+    {
+        /// <summary>
+        /// Converts a DateTimeOffset value to a long.
+        /// </summary>
+        /// <param name="value">DateTimeOffset value to convert.</param>
+        /// <returns>Long integer value containing the number of ticks for
+        /// the DateTimeOffset in UTC.</returns>
+        public static long Convert(DateTimeOffset value, long unused)
+        {
+            return value.UtcTicks;
+        }
+
+        /// <summary>
+        /// Converts a long value to a DateTimeOffset value.
+        /// </summary>
+        /// <param name="value">Long value to convert. This value must
+        /// be in UTC.</param>
+        /// <param name="unused"></param>
+        /// <returns>DateTimeOffset</returns>
+        public static DateTimeOffset Convert(long value, DateTimeOffset unused)
+        {
+            return new DateTimeOffset(value, TimeSpan.Zero);
+        }
+    }
+}
