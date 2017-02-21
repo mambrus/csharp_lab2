4f24a32b52fb4b924742875cdbe0ebb15abc08e1
Michael Ambrus <michael.ambrus@medius.se>
2017-02-21 16:26:47 +0100

    Part #22 - The core of the stateful service
    * Implements the interface IVotingService
    
      public long RequestCount
      public Task AddVoteAsync()
      public Task<IReadOnlyList<VotingData>> GetVotingDataAsync()
    
    * Corrects error in text (required usage of ref)
    
    * Reverts previously made dummy functions

--- Voting/VotingState/VotingState.cs
+++ Voting/VotingState/VotingState.cs
@@ -33,29 +33,6 @@ namespace VotingState
             adapter.StateManager = this.StateManager;
         }
 
-        public long RequestCount
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public Task AddVoteAsync(string key, int count, string id, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IReadOnlyList<VotingData>> GetVotingDataAsync(string id, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
         /// <summary>
         /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) 
         /// for this service replica to handle client or user requests.
@@ -87,6 +64,85 @@ namespace VotingState
             {
                 cancellationToken.ThrowIfCancellationRequested();
                 await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+
+        /// <summary>
+        /// Gets the count dictionary. If it doesn't already exist it is created.
+        /// </summary>
+        /// <remarks>This is a good example of how to initialize reliable collections. 
+        /// Rather than initializing and caching a value, this approach will work on 
+        /// both primary and secondary replicas (if secondary reads are enabled.</remarks>
+        private async Task<IReliableDictionary<string, VotingData>> GetCountDictionaryAsync()
+        {
+            return await StateManager.GetOrAddAsync<IReliableDictionary<string, VotingData>>(
+                "votingCountDictionary").ConfigureAwait(false);
+        }
+
+        // Track the number of requests to the controller.
+        private long _requestCount = 0;
+
+        // Returns or increments the request count.
+        public long RequestCount
+        {
+            get { return Volatile.Read(ref _requestCount); }
+            set { Interlocked.Increment(ref _requestCount); }
+        }
+
+        /// <summary>
+        /// Gets the list of VotingData items.
+        /// </summary>
+        public async Task<IReadOnlyList<VotingData>> GetVotingDataAsync(string id, CancellationToken token)
+        {
+            List<VotingData> items = new List<VotingData>();
+            ServiceEventSource.Current.ServiceRequestStart("VotingState.GetVotingDataAsync", id);
+
+            // Get the dictionary.
+            var dictionary = await GetCountDictionaryAsync();
+            using (ITransaction tx = StateManager.CreateTransaction())
+            {
+                // Create the enumerable and get the enumerator.
+                var enumItems = (await dictionary.CreateEnumerableAsync(tx)).GetAsyncEnumerator();
+                while (await enumItems.MoveNextAsync(token))
+                {
+                    items.Add(enumItems.Current.Value);
+                    if (items.Count > 1000)
+                        break;
+                }
+            }
+
+            return items;
+        }
+
+        /// <summary>
+        /// Updates the count on a vote or adds a new vote if it doesn't already exist.
+        /// </summary>
+        public async Task AddVoteAsync(string key, int count, string id, CancellationToken token)
+        {
+            ServiceEventSource.Current.ServiceRequestStart("VotingState.AddVoteAsync", id);
+
+            // Get the dictionary.
+            var dictionary = await GetCountDictionaryAsync();
+            using (ITransaction tx = StateManager.CreateTransaction())
+            {
+                // Try to get the existing value
+                ConditionalValue<VotingData> result = await dictionary.TryGetValueAsync(tx, key, LockMode.Update);
+                if (result.HasValue)
+                {
+                    // VotingData is immutable to ensure the reference returned from the dictionary
+                    // isn't modified. If it were not immutable, you changed a field’s value, and then the transaction aborts, the value will 
+                    // remain modified in memory, corrupting your data.
+                    VotingData newData = result.Value.UpdateWith(result.Value.Count + count);
+                    await dictionary.TryUpdateAsync(tx, key, newData, result.Value);
+                }
+                else
+                {
+                    // Add a new VotingData item to the collection
+                    await dictionary.AddAsync(tx, key, new VotingData(key, count, count, DateTimeOffset.Now));
+                }
+
+                // Commit the transaction.
+                await tx.CommitAsync();
             }
         }
     }
