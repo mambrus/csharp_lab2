ba8ff512c0b351a9e62415eaa5c27566b4c20b82
Michael Ambrus <michael.ambrus@medius.se>
2017-02-22 16:02:05 +0100

    Part #25 - #28 - Replace Get, Post & Delete
    This commit needs to be either all parts (#25 - #28) together, or
    or #25 would need to be split into two parts as follows:
    
     #25A: Adding the new HttpClient
     #26 Get
     #27 Post
     #28 Delete
     #25 Finally remove the key-data this whole lab is all about. I.e. keeping
         count of votes, which was stored in the "static _counts" list.
    
    Look at the new local container "votes" in "Get"

--- Voting/VotingService/Controllers/VotesController.cs
+++ Voting/VotingService/Controllers/VotesController.cs
@@ -18,66 +18,69 @@
         // Used for health checks.
         public static long _requestCount = 0L;
 
-        // Holds the votes and counts. NOTE: THIS IS NOT THREAD SAFE FOR THE PURPOSES OF THE LAB ONLY.
-        static Dictionary<string, int> _counts = new Dictionary<string, int>();
+        HttpClient _client = new HttpClient();
 
-        // GET api/votes 
+        // GET api/votes
         [HttpGet]
         [Route("api/votes")]
-        public HttpResponseMessage Get()
+        public async Task<HttpResponseMessage> Get()
         {
-            string activityID = Guid.NewGuid().ToString();
-            ServiceEventSource.Current.ServiceRequestStart("VotesController.Get", activityID);
+            string activityId = Guid.NewGuid().ToString();
+            ServiceEventSource.Current.ServiceRequestStart(
+                "VotesController.Get",
+                activityId);
 
             Interlocked.Increment(ref _requestCount);
 
-            List<KeyValuePair<string, int>> votes = new List<KeyValuePair<string, int>>(_counts.Count);
-            foreach (KeyValuePair<string, int> kvp in _counts)
-            {
-                votes.Add(kvp);
-            }
+            string url = $"http://localhost:19081/Voting/VotingState/api/votes?PartitionKey=0&PartitionKind=Int64Range";
+            HttpResponseMessage msg = await _client.GetAsync(url).ConfigureAwait(false);
+            string json = await msg.Content.ReadAsStringAsync().ConfigureAwait(false);
+            List<KeyValuePair<string, int>> votes = 
+                JsonConvert.DeserializeObject<List<KeyValuePair<string, int>>>(json);
 
             var response = Request.CreateResponse(HttpStatusCode.OK, votes);
             response.Headers.CacheControl = new CacheControlHeaderValue() { NoCache = true, MustRevalidate = true };
+
+            ServiceEventSource.Current.ServiceRequestStop(
+                "VotesController.Get",
+                activityId);
             return response;
         }
 
         [HttpPost]
         [Route("api/{key}")]
-        public HttpResponseMessage Post(string key)
+        public async Task<HttpResponseMessage> Post(string key)
         {
-            string activityID = Guid.NewGuid().ToString();
-            ServiceEventSource.Current.ServiceRequestStart("VotesController.Post", activityID);
+            string activityId = Guid.NewGuid().ToString();
+            ServiceEventSource.Current.ServiceRequestStart("VotesController.Post", activityId);
 
             Interlocked.Increment(ref _requestCount);
 
-            if (false == _counts.ContainsKey(key))
-            {
-                _counts.Add(key, 1);
-            }
-            else
-            {
-                _counts[key] = _counts[key] + 1;
-            }
+            string url = $"http://localhost:19081/Voting/VotingState/api/{key}?PartitionKey=0&PartitionKind=Int64Range";
+            HttpResponseMessage msg = await _client.PostAsync(url, null).ConfigureAwait(false);
 
-            return Request.CreateResponse(HttpStatusCode.NoContent);
+            ServiceEventSource.Current.ServiceRequestStop(
+                "VotesController.Post",
+                activityId);
+            return Request.CreateResponse(msg.StatusCode);
         }
 
         [HttpDelete]
         [Route("api/{key}")]
         public HttpResponseMessage Delete(string key)
         {
-            string activityID = Guid.NewGuid().ToString();
-            ServiceEventSource.Current.ServiceRequestStart("VotesController.Detete", activityID);
+            string activityId = Guid.NewGuid().ToString();
+            ServiceEventSource.Current.ServiceRequestStart(
+                "VotesController.Delete",
+                activityId);
 
             Interlocked.Increment(ref _requestCount);
 
-            if (true == _counts.ContainsKey(key))
-            {
-                if (_counts.Remove(key))
-                    return Request.CreateResponse(HttpStatusCode.OK);
-            }
+            // Ignoring delete for this lab.
 
+            ServiceEventSource.Current.ServiceRequestStop(
+                "VotesController.Delete",
+                activityId);
             return Request.CreateResponse(HttpStatusCode.NotFound);
         }
 
